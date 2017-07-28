8943b4cac10ca502f40ade066f1307115c577368
Shekhar <pythonic@gmail.com>
2017-07-28 09:50:18 +0530

    added one more

--- fbdata.py
+++ fbdata.py
@@ -1,5 +1,5 @@
 PAGES = ['youthhostelassociationofindia', '569602886513482', 'Trekfreaks', 'PuneRunning', 'grassroutes.journeys', 'Foliagers', '233136143414695', 'TREKDI', 'spitiadventure', 'lafl2012',
          'PlacesAroundPune', 'BhatkantiSahyadrichyaKushit', 'trekthehimalayas', 'WhiteMagicAdventure',
          'thewesternroutes', 'sgtrekkers', 'makemytrek', 'plusvalleyadventure', 'SanjayGandhiNationalPark',
-         'themountainhoppers']
+         'themountainhoppers', 'xtremetrekkers']
 PAGES_didnt_work = ['shivdurg', '317771368415795'] # rockface
