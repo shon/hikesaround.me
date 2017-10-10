b5d14b0614fc6a2c2c0b591b9fda18d6594237df
Shekhar <pythonic@gmail.com>
2017-10-10 21:07:01 +0530

    RawAdventureSolutions

--- fbdata.py
+++ fbdata.py
@@ -1,5 +1,5 @@
 PAGES = ['youthhostelassociationofindia', '569602886513482', 'Trekfreaks', 'PuneRunning', 'grassroutes.journeys', 'Foliagers', '233136143414695', 'TREKDI', 'spitiadventure', 'lafl2012',
          'PlacesAroundPune', 'BhatkantiSahyadrichyaKushit', 'trekthehimalayas', 'WhiteMagicAdventure',
          'thewesternroutes', 'sgtrekkers', 'makemytrek', 'plusvalleyadventure', 'SanjayGandhiNationalPark',
-         'themountainhoppers', 'xtremetrekkers', '3pointadventure', 'gadvede', 'trekmatesindia']
+         'themountainhoppers', 'xtremetrekkers', '3pointadventure', 'gadvede', 'trekmatesindia', 'RawAdventureSolutions']
 PAGES_didnt_work = ['shivdurg', '317771368415795'] # rockface
