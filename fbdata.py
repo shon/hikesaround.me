0f47b920cc9843293c6cb51744f3a6b47628591a
Shekhar <pythonic@gmail.com>
2017-10-25 13:40:38 +0530

    added one more host

--- fbdata.py
+++ fbdata.py
@@ -1,5 +1,6 @@
 PAGES = ['youthhostelassociationofindia', '569602886513482', 'Trekfreaks', 'PuneRunning', 'grassroutes.journeys', 'Foliagers', '233136143414695', 'TREKDI', 'spitiadventure', 'lafl2012',
          'PlacesAroundPune', 'BhatkantiSahyadrichyaKushit', 'trekthehimalayas', 'WhiteMagicAdventure',
          'thewesternroutes', 'sgtrekkers', 'makemytrek', 'plusvalleyadventure', 'SanjayGandhiNationalPark',
-         'themountainhoppers', 'xtremetrekkers', '3pointadventure', 'gadvede', 'trekmatesindia', 'RawAdventureSolutions']
+         'themountainhoppers', 'xtremetrekkers', '3pointadventure', 'gadvede', 'trekmatesindia', 'RawAdventureSolutions',
+         'decathlonwagholi']
 PAGES_didnt_work = ['shivdurg', '317771368415795'] # rockface
