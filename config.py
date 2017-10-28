4009519b921d9ab398612fe762878cf919759a85
Rishi Ranjan <rishi@scroll.in>
2017-10-28 15:28:36 +0530

    Adding manifest.json

---
+++ config.py
@@ -0,0 +1,6 @@
+MANIFEST = {
+    'NAME': 'Hikes Around Me',
+    'SHORT_NAME': 'Hikes Around Me',
+    'DESCRIPTION': 'Helps you find hikes, treks, adventures around Pune and Mumbai. Listing platform for organizers.',
+    'ANDROID_APP_ID': 'io.gonative.android.rqbaw'
+}
